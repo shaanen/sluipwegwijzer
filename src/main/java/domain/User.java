b58339ee8b9f4d7d0e4019e75c6750a52f4a0a0d
Sjors Haanen <s.haanen@student.fontys.nl>
2015-12-10 11:27:59 +0100

    Extended macs

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -18,12 +18,14 @@ public class User {
     private String accessToken;
     private String refreshToken;
     private List<User> friends;
+    private final List<String> macs;
 
-    public User(int userId, String name, String accessToken, String refreshToken) {
+    public User(int userId, String name, String accessToken, String refreshToken, List<String> macs) {
         this.userId = userId;
         this.name = name;
         this.accessToken = accessToken;
         this.refreshToken = refreshToken;
+        this.macs = macs;
     }
 
     public int getUserId() {
@@ -45,6 +47,10 @@ public class User {
     public List<User> getFriends() {
         return friends;
     }
+    
+    public List<String> getMacs() {
+        return macs;
+    }
 
     public void setAccessToken(String accessToken) {
         this.accessToken = accessToken;
@@ -57,4 +63,8 @@ public class User {
     public void addFriend(User friend){
         friends.add(friend);
     }
+    
+    public void addMac(String mac){
+        this.macs.add(mac);
+    }    
 }
